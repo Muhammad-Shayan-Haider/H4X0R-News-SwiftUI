e0e0477713d70c1be90eba8894f55e4257de2795
Muhammad Shayan <shayanhaider1999@gmail.com>
2022-10-30 11:21:21 +0500

    Initial Commit

---
+++ H4X0R-News-SwiftUI/H4X0R_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_SwiftUIApp.swift
+//  H4X0R-News-SwiftUI
+//
+//  Created by Muhammad Shayyan on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
